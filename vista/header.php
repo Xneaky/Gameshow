5b9a4d5c0c2dfb0c835a2172c04de537a7cbc48c
Javier Castillo <castillo_5326@hotmail.com>
2018-10-04 20:03:17 -0600

    header reconoce usuario conectado

--- vista/header.php
+++ vista/header.php
@@ -1,3 +1,16 @@
+<?php
+$header1 =
+'<div class="nav-login">
+    <form action="" method="post">
+        <input type="text" name="usuario" placeholder="Usuario">
+        <input type="password" name="pwd" placeholder="Contrasena">
+        <button type="submit" name="submit">Login</button>
+    </form>
+    <a href="../vista/registro-vista.php">Registrarse</a>
+</div>';
+
+$header2 = ''
+?>
 <!DOCTYPE html>
 <html>
 
@@ -13,22 +26,24 @@
 
 <body>
 
-<header>
-    <nav>
-        <div class="main-wrapper">
-            <ul>
-                <li><a href="../index.php">Home</a></li>
-            </ul>
-            <div class="nav-login">
-                <form action="" method="post">
-                    <input type="text" name="usuario" placeholder="Usuario">
-                    <input type="password" name="pwd" placeholder="Contrasena">
-                    <button type="submit" name="submit">Login</button>
-                </form>
-                <a href="../vista/registro-vista.php">Registrarse</a>
+    <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                </ul>
+
+
+                <?php
+        if(!isset($_SESSION['usuario'])){
+        echo $header1;
+    }else{
+            echo $header2;
+        }
+     ?>
+
             </div>
-        </div>
-    </nav>
+        </nav>
 
 
-</header>
+    </header>
