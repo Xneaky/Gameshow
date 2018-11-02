bcbdd7faaaaded6d777c0d9ee9b52a52983b2bca
Javier Castillo <castillo_5326@hotmail.com>
2018-11-02 16:29:37 -0600

    Registro de usuarios regulares

---
+++ app/appRegistro.js
@@ -0,0 +1,74 @@
+'use strict';
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+app.controller('loginCtrl', function ($scope, $uibModal, $http) {
+    $scope.usuarios = [];
+    $scope.roles = [];
+
+    $scope.nuevoUsuario = function(size) {
+        $scope.modalInstance = $uibModal.open({
+            backdrop: 'static',
+            scope: $scope,
+            keyboard: false,
+            templateUrl: 'views/usuarios/modalRegistroUsuario.html',
+            controller: 'crearUsuarioCtrl',
+            size: size
+        });
+    };
+
+
+});
+
+app.controller('crearUsuarioCtrl', function ($scope, $http, $uibModal, $uibModalInstance, $window) {
+    $scope.newUsuario = {};
+
+    var administrarMensajeSweet = function(conf) {
+        $window.swal({
+                title: conf.titulo,
+                text: conf.texto,
+                type: conf.tipo,
+                showCancelButton: false
+            },
+            function(isConfirm){
+                if (isConfirm)
+                    $uibModalInstance.close();
+            });
+    };
+
+    $scope.guardar = function(newUsuario) {
+
+        var stringQuery = "INSERT INTO usuarios (idrol, nombre, apellido, email, username, pwd, activo) VALUES " +
+            "('3'," +
+            "'" + newUsuario.nombre_usuario + "'," +
+            "'" + newUsuario.apellido_usuario + "'," +
+            "'" + newUsuario.email + "'," +
+            "'" + newUsuario.username + "'," +
+            "'" + newUsuario.pwd + "', true)";
+
+        var consulta = {
+            query: stringQuery,
+            method: "POST"
+        }
+
+        $http.post('apis/porcesaAPI.php', {
+            data: {params:  consulta}
+        }).success(function(response){
+            if (response == "1") {
+                administrarMensajeSweet({titulo:'Usuario ingresado', tipo:'success', texto: ''});
+            } else {
+                administrarMensajeSweet({titulo:'Error al ingresar', tipo:'error', texto: ''});
+            }
+        }).error(function(){
+            administrarMensajeSweet({titulo:'Error al enviar params', tipo:'error', texto: ''});
+        });
+
+    };
+
+    $scope.cerrarModal = function() {
+        $uibModalInstance.close();
+    };
+});
+
+app.directive('menuApp', function($parse) {
+
+});
